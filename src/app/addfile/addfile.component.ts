fa228dfee0c59c4c00b8a7b19b0ede4d62c4a16f
khaled-baili <bailikhaled@gmail.com>
2022-12-02 00:59:03 +0100

    add form component

---
+++ src/app/addfile/addfile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addfile',
+  templateUrl: './addfile.component.html',
+  styleUrls: ['./addfile.component.css']
+})
+export class AddfileComponent {
+
+}
