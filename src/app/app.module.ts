1b0f7f9bfcb1867b0359abc2ff3b678221407b72
khaled-baili <bailikhaled@gmail.com>
2022-12-02 00:58:17 +0100

    add form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddfileComponent } from './addfile/addfile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddfileComponent
   ],
   imports: [
     BrowserModule
